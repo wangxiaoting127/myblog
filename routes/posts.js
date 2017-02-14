82b91ec8ededebf9c5aa0602d88fbf316ac1da26
wangxiaoting127 <wangxiaoting@oopsdata.com>
2017-02-14 17:13:45 +0800

    hahaha

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,19 @@ var checkLogin = require('../middlewares/check').checkLogin;
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', function(req, res, next) {
- res.render('posts');
+ var author=req.query.author;
+ PostModel.getPosts(author)
+  .then(function(posts){
+    res.render('posts',{
+      posts:posts
+    })
+  })
+  .catch(next)
+});
+
+// GET /posts/create 发表文章页
+router.get('/create', checkLogin, function(req, res, next) {
+  res.render('create')
 });
 
 // POST /posts 发表一篇文章
@@ -15,12 +27,9 @@ router.post('/', checkLogin, function(req, res, next) {
  var author=req.session.user._id;
  var title=req.fields.title;
  var content=req.fields.content;
-});
 
-// GET /posts/create 发表文章页
-router.get('/create', checkLogin, function(req, res, next) {
-  res.render('create')
-});
+
+
 
 
 try{
@@ -43,14 +52,17 @@ var post={
   pv:0
 };
 PostModel.create(post)
-  .then(function(){
+  .then(function(result){
     // 此 post 是插入 mongodb 后的值，包含 _id
+    console.log(result);
     post=result.ops[0];
+   
     req.flash('success','发表成功')
     // 发表成功后跳转到该文章页
-    res.redirect('/posts/${post_id}')
+    res.redirect('/posts/${post._id}')
   })
   .catch(next);
+  });
 
 
 
